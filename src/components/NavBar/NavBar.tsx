9586bec53536e8b01820ee14ffe9cbd05b80a424
Lorenz De Robles <pvd8@txstate.edu>
2024-05-02 15:44:14 -0500

    organize some stuff

--- src/components/NavBar/NavBar.tsx
+++ src/components/NavBar/NavBar.tsx
@@ -10,27 +10,27 @@ function NavBar()
          <a className="navbar-brand">Lorenz De Robles</a>
          <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
             <span className="navbar-toggler-icon"></span>
-    </button>
+         </button>
     
-    <div className="collapse navbar-collapse" id="navbarNav">
-      <ul className="navbar-nav">
+        <div className="collapse navbar-collapse" id="navbarNav">
+          <ul className="navbar-nav">
 
-        <li className="nav-item">
-              <Link className="nav-link active" to="/website/blog">Blog</Link>
-        </li>
-        
-        <li className="nav-item">
-          <a className="nav-link" href="https://github.com/lorenzjdr">GitHub</a>
-        </li>
+            <li className="nav-item">
+                  <Link className="nav-link active" to="/website/blog">Blog</Link>
+            </li>
+            
+            <li className="nav-item">
+              <a className="nav-link" href="https://github.com/lorenzjdr">GitHub</a>
+            </li>
 
-        <li className="nav-item">
-          <a className="nav-link" href= "https://www.linkedin.com/in/lorenz-de-robles/" >LinkedIn</a>
-        </li>
+            <li className="nav-item">
+              <a className="nav-link" href= "https://www.linkedin.com/in/lorenz-de-robles/" >LinkedIn</a>
+            </li>
 
-      </ul>
-    </div>
-  </div>
-</nav>
+          </ul>
+        </div>
+      </div>
+    </nav>
    </> 
    );  
 }
