1348d2f983666b1027d674d2568ea2258991e1ea
Lorenz De Robles <pvd8@txstate.edu>
2024-05-02 17:54:14 -0500

    added some style

--- src/Pages/Blog/BlogHome.tsx
+++ src/Pages/Blog/BlogHome.tsx
@@ -1,17 +1,18 @@
 import { Link } from "react-router-dom";
+import './BlogHome.css';
 
 const BlogHome = () => {
   return (
-    <div >
-      <h1>Welcome to My Blog!</h1>
+    <div className="blog-body" >
+      <h1>Hi welcome to my thoughts!</h1>
 
-      <section>
+      <section className="section">
         <h2>Hopefully I write stuff here lol</h2>
 
-        <ul>
-          <li><Link to="/website/blog/1">Travel Blog 1</Link></li>
+        <ul className="list">
+          <li><Link to="/website/blog/1">Blog 1</Link></li>
         </ul>
-        
+
       </section>
 
     </div>
