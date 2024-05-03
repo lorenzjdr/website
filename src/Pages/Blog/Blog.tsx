dcf975a3f55200913c25958a6a418252eb5a8c3c
Lorenz De Robles <pvd8@txstate.edu>
2024-05-03 00:27:17 -0500

    added some sytling

--- src/Pages/Blog/Blog.tsx
+++ src/Pages/Blog/Blog.tsx
@@ -1,22 +1,23 @@
 import { useParams } from 'react-router-dom';
+import './Blog.css'
 
 const Blog = () =>
 {
    const {id} = useParams();
-  return (
-    <div className="blog-post">
-      
-      <div className="blog-title">
-        <h1>title</h1>
-        <p>By  - </p>
-      </div>
+   return (
+     <div className="blog-post">
 
-        <div className = "blog-content">
-          <p>content</p>
-        </div>
+       <div className="blog-title">
+         <h1>title</h1>
+         <p>By  - </p>
+       </div>
 
-    </div>
-  );
+         <div className = "blog-content">
+           <p>content</p>
+         </div>
+
+     </div>
+   );
 }
 
 export default Blog;
