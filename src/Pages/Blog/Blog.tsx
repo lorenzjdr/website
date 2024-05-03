a33b3a780dbb618da41c55e63c44e1ef6287c062
Lorenz De Robles <pvd8@txstate.edu>
2024-05-03 00:24:08 -0500

    Added stuff to change later

--- src/Pages/Blog/Blog.tsx
+++ src/Pages/Blog/Blog.tsx
@@ -1,9 +1,23 @@
+import { useParams } from 'react-router-dom';
 
 const Blog = () =>
 {
-   return (
-      <h1>Under Construction</h1>
-   );
+   const {id} = useParams();
+  return (
+    <div className="blog-post">
+      
+      <div className="blog-title">
+        <h1>title</h1>
+        <p>By  - </p>
+      </div>
+
+        <div className = "blog-content">
+          <p>content</p>
+        </div>
+
+    </div>
+  );
 }
 
 export default Blog;
+
