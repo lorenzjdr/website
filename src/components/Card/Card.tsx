8f437f7184ea4f1d9c204760a0f71bfba0269f86
Lorenz De Robles <pvd8@txstate.edu>
2024-10-09 21:37:47 -0500

    Added Props to the Card Class

--- src/components/Card/Card.tsx
+++ src/components/Card/Card.tsx
@@ -1,19 +1,24 @@
-import './Card.css'
+import './Card.css';
 
-const Card = () =>{
+interface CardProps {
+  title: string;
+  description: string;
+  image: string;
+  link: string;
+}
+
+const Card = ({ title, description, image, link }: CardProps) => {
   return (
     <div className="card">
       <img 
-        src=""
+        src={image}
         alt="Image" 
         className="card-image" 
       />
       <div className="card-body">
-        <h3 className="card-title">Card Title</h3>
-        <p className="card-text">
-            Description and etc
-        </p>
-        <a className="" href="demo/link">Demo</a>
+        <h3 className="card-title">{title}</h3>
+        <p className="card-text">{description}</p>
+        <a className="card-link" href={link}>Demo</a>
       </div>
     </div>
   );
