6fed142aa3f7e53f361fa9eb59315f7ba50089cd
lorenzjdr <lorenz.jdr@gmail.com>
2024-10-10 21:31:24 -0500

    Custom hook for component to appear when scrolled to

---
+++ src/Utils/visiblility.tsx
@@ -0,0 +1,28 @@
+import { useState, useEffect, useRef } from 'react';
+
+const useVisibility = () => {
+  const [isVisible, setIsVisible] = useState(false);
+  const ref = useRef<HTMLDivElement | null>(null);
+
+  useEffect(() => {
+    const observer = new IntersectionObserver((entries) => {
+      entries.forEach(entry => {
+        setIsVisible(entry.isIntersecting);
+      });
+    });
+
+    if (ref.current) {
+      observer.observe(ref.current);
+    }
+
+    return () => {
+      if (ref.current) {
+        observer.unobserve(ref.current);
+      }
+    };
+  }, []);
+
+  return { ref, isVisible };
+};
+
+export default useVisibility;
