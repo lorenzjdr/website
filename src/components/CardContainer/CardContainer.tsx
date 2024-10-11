90bc3fd6e8a4b7e34310740935f52a400a0cb051
lorenzjdr <lorenz.jdr@gmail.com>
2024-10-11 18:55:41 -0500

    Added the fade-in animation to the container when it is visible

--- src/components/CardContainer/CardContainer.tsx
+++ src/components/CardContainer/CardContainer.tsx
@@ -1,14 +1,37 @@
+import useVisibility from "../../Utils/visiblility";
 import Card from "../Card/Card";
-import "./CardContainer.css"
+import "./CardContainer.css";
 
-const CardContainer = () =>{
-  return (
-    <div className="card-rows">
-      <Card title={"Danddy"} description={"A D&D Campaign helper that improves quality of life for campaigns and combat."} image={""} link={""}/>
-      <Card title={"TamaGOtchi"} description={"Habit tracker written in Golang with Tamagotchi gameplay elements."} image={""} link={""}/>
-      <Card title={"VTT_to_TXT"} description={"Convert a VTT file to TXT."} image={""} link={"https://github.com/lorenzjdr/VTT_to_TXT"}/>
-    </div>
-  );
+const CardContainer = () => {
+   const { ref, isVisible} = useVisibility();
+   return (
+     <div 
+       ref={ref} 
+       className={`card-rows ${isVisible ? 'visible' : 'hidden'}`}
+       style={{ transition: 'opacity 0.5s', opacity: isVisible ? 1 : 0 }}
+     >
+
+       <Card 
+         title={"Danddy"} 
+         description={"A D&D Campaign helper that improves quality of life for campaigns and combat."} 
+         image={""} 
+         link={""}
+       />
+   
+       <Card 
+         title={"TamaGOtchi"} 
+         description={"Habit tracker written in Golang with Tamagotchi gameplay elements."} 
+         image={""} 
+         link={""}
+       />
+       <Card 
+         title={"VTT_to_TXT"} 
+         description={"Convert a VTT file to TXT."} 
+         image={""} 
+         link={"https://github.com/lorenzjdr/VTT_to_TXT"}
+       />
+     </div>
+   );
 }
 
 export default CardContainer;
