47a6c940b929e08368a8b4a4ce0e1a220332e4eb
lorenzjdr <lorenz.jdr@gmail.com>
2025-03-27 18:32:48 -0500

    Added new variables for color

--- src/components/NavBar/NavBar.tsx
+++ src/components/NavBar/NavBar.tsx
@@ -6,7 +6,7 @@ function NavBar()
    <>
    <nav className="navbar navbar-expand-lg ">
       <div className="container-fluid">
-         <a className="navbar-brand">Lorenz De Robles</a>
+         <a className="navbar-brand" href='/'>Lorenz De Robles</a>
          <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
             <span className="navbar-toggler-icon"></span>
          </button>
